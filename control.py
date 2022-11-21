9fcfe5c05acc91fc19585ce3e502e81a6f43e7f6
LAPTOP-RRIK1H8B\tommy <s108213016@mail1.ncnu.edu.tw>
2022-11-21 15:51:44 +0800

    競標 Upload

--- control.py
+++ control.py
@@ -4,7 +4,14 @@ from dbConfig import conn, cur
 
 def getAuctionList():
     #拍賣首頁
-    sql="select id, name,price from auctionlist order by id asc ;"
+    sql="SELECT `id`, `name`, `price`, `uName` FROM `auctionlist` order by id asc;"
     cur.execute(sql)
     records = cur.fetchall()
     return records
+    
+def bidding(uid,id,bidding):
+    #拍賣首頁
+    sql="UPDATE `auctionlist` SET `price`=%s,`uName`= (select uName from user where uid = %s) WHERE id = %s and price < %s;"
+    cur.execute(sql,(bidding,uid,id,bidding))
+    conn.commit()
+    return True
