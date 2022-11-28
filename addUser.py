237b1862630ba32500996544596ef4427785a7ef
LAPTOP-RRIK1H8B\tommy <s108213016@mail1.ncnu.edu.tw>
2022-11-28 16:07:24 +0800

    加入會員

---
+++ addUser.py
@@ -0,0 +1,36 @@
+#!C:\Users\tommy\AppData\Local\Programs\Python\Python310\python.exe
+#-*- coding: utf-8 -*-
+#處理stdio輸出編碼，以避免亂碼
+import codecs, sys 
+sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+import cgi
+import control as ctrl
+#連線DB
+from dbConfig import conn, cur
+#先印出http 表頭
+print("Content-Type: text/html; charset=utf-8\n")
+print("""
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>範例1</title>
+<style type="text/css">
+body {width:600px; margin:10px auto;}
+#content {background-color:DarkOliveGreen; margin:20px; padding:20px; border:2px solid ivory; font-size: 14pt; color:#ff9; line-height:28px}
+a {color: yellow}
+</style>
+</head>
+<body>
+<div id="content">
+""")
+
+#查詢
+form = cgi.FieldStorage()
+name=form.getvalue('name') 
+
+ctrl.addUser(name)
+
+print("歡迎!")
+print("<br><a href='auctionMenu.py'>確認</a>")
+print("</body></html>")
